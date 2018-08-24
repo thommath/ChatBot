8a608dfbf9fed0c5b409dfc2aedca98ba7fc7c39
Thommath <thomlmath@gmail.com>
2018-08-24 12:06:09 +0200

    Chatter can now add training phrases to intents #6

--- dialogflow.js
+++ dialogflow.js
@@ -1,4 +1,4 @@
-// Instantiate a DialogFlow client.
+const uuidv4 = require('uuid/v4');// Instantiate a DialogFlow client.
 const dialogflow = require('dialogflow');
 
 
@@ -10,10 +10,13 @@ class Dialogflow {
         this.languageCode = 'en-US';
         
         this.sessionClient = new dialogflow.SessionsClient();
+        this.intentsClient = new dialogflow.IntentsClient();
+        this.formattedParent = this.intentsClient.projectAgentPath(projectId);
 
         // Define session path
         this.sessionPath = this.sessionClient.sessionPath(projectId, sessionId);
 
+        this.getIntentList();
     }
 
     detectIntent(msg) {
@@ -35,28 +38,76 @@ class Dialogflow {
         return this.sessionClient
             .detectIntent(request)
             .then(responses => {
-                console.log('Detected intent');
                 const result = responses[0].queryResult;
-                console.log(`  Query: ${result.queryText}`);
-                console.log(`  Response: ${result.fulfillmentText}`);
-                if (result.intent) {
-                    console.log(`  Intent: ${result.intent.displayName}`);
+                if (result.intent && result.intent.displayName !== 'Default Fallback Intent') {
+
+                    if (this.unknownPhrase) {
+                        const intent = this.addTrainingPhrase(this.unknownPhrase, this.getIntent(result.intent.name));
+                        intent.then(console.log)
+                        return 'Phrase Learned';
+                    }
+
                     return result.intent.displayName;
                 } else {
                     console.log(`  No intent matched.`);
-                    return '';
+                    this.unknownPhrase = msg;
+                    return `I'm sorry but I'm not sure what that means, can you rephrase it please?`;
                 }
             })
             .catch(err => {
                 console.error('ERROR:', err);
             });
     }
+
+    getIntentList() {
+        // The text query request.
+        const request = {
+            parent: this.formattedParent,
+            intentView: 'INTENT_VIEW_FULL',
+        };
+        
+        // Send request and log result
+        return this.intentsClient
+            .listIntents(request)
+            .then(response => {
+                const result = response;
+                this.intentList = response[0];
+                console.log(response[0])
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
+    }
+
+    getIntent(name) {
+        return this.intentList.find(i => i.name === name);
+    }
+
+
+    addTrainingPhrase(phrase, intent) {
+        const request = {
+            intent: Object.assign(intent.name, { trainingPhrases: intent.trainingPhrases.concat([{name: uuidv4(), type: 'EXAMPLE', parts: [{ text: phrase }] }]) })
+        };
+        console.log('learned ' + phrase)
+        return this.intentsClient
+            .updateIntent(request);
+    }
+
+    //updateIntent(intent, )
 };
+
 
 exports.Dialogflow = Dialogflow;
 
 const df = new Dialogflow();
-df.detectIntent('hi').then(console.log);
+df.getIntentList().then(console.log);
+df.detectIntent('What is my last transaction?').then(() => 
+    df.detectIntent('Something unknown').then(console.log))
+.then(() => 
+    df.detectIntent('Something unknown').then(console.log))
+.then(() => 
+    df.detectIntent('What is my last transaction?').then(console.log))
+
 
 
 
